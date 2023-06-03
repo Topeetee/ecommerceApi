46e803ccd24e2e5df376ecf9ee85f6336dd661c9
Topeetee <temitopeakinwekomi28@gmail.com>
2023-06-03 14:59:50 -0700

    worked on cart

--- controller/cart.js
+++ controller/cart.js
@@ -1,13 +1,53 @@
-//  const Cart = require("../models/cart")
-//  const createError = require("../utils/error")
+ const Cart = require("../models/cart")
+ const createError = require("../utils/error")
 
-//  const createCart = async(req,res,next)=>{
-//     const cart = new Cart(req.body);
-//     try{
-//         const savedCart = await cart.save()
-//         res.status(200).json(savedCart)
-//     }
-//     catch(err){
-//         next(err)
-//     }
-//  }
+ const createCart = async(req,res,next)=>{
+    const cart = new Cart(req.body);
+    try{
+        const savedCart = await cart.save()
+        res.status(200).json(savedCart)
+    }
+    catch(err){
+        next(err)
+    }
+ }
+ const updateCart = async(req,res,next)=>{
+    try { 
+        const updatedCart = await Cart.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedProduct);
+      } catch (err) {
+        next(res.status(500).json(err));
+      }
+ }
+ const deleteCart = async(req,res,next)=>{
+    try{
+        await Product.findByIdAndDelete(req.params.id)
+            res.status(200).json("product has been deleted")
+    }catch (err) {
+        next(res.status(500).json(err));
+      }
+}
+const getCart = async(req,res,next)=>{
+    try{
+        const getcart = await Cart.findById(req.params.id);
+        res.status(200).json(getcart)
+    }catch (err) {
+        next(res.status(500).json(err));
+      }
+}
+ const getCarts = async (req, res) => {
+    try {
+      const carts = await Cart.find();
+      res.status(200).json(carts);
+    } catch (err) {
+        next(res.status(500).json(err));
+    }
+  };
+
+  module.exports = {createCart, updateCart,deleteCart, getCart, getCarts}
--- routes/cart.js
+++ routes/cart.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const {verifyUser,verifyAdmin} = require("../utils/verifyToken")
-// const {createCart}= require("../controller/cart")
+const {verifyUser,verifyAdmin} = require("../utils/verifyToken")
+const {createCart, deleteCart, getCart, getCarts, updateCart}= require("../controller/cart")
 
-// router.post("/addToCart", verifyUser, createCart)
-// //update
-// router.put("/:id", verifyUser,updateCart)
-// //delete
-// router.delete("/:id", verifyUser,deleteCart)
-// //get specific hotel
-// router.get("/:id", verifyUser, getCart)
+router.post("/addToCart", verifyUser, createCart)
+//update
+router.put("/:id", verifyUser,updateCart)
+//delete
+router.delete("/:id", verifyUser,deleteCart)
+//get specific hotel
+router.get("/:id", verifyUser , getCart)
 
-// //get all hotels
-// router.get("/", verifyAdmin,getCarts)
+//get all hotels
+router.get("/", verifyAdmin,getCarts)
 module.exports = router;
